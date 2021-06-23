d16caf4f0ddef0aa5a9b645897acd944b649b8ab
bryanagamk <bryan@kyodo-i.com>
2021-06-23 21:55:42 +0700

    stack

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// Stack represents stack that hold a slice
+type Stack struct {
+	items []int
+}
+
+// Push
+func (s *Stack) Push (i int) {
+	s.items = append(s.items, i)
+}
+
+// Pop
+func (s *Stack) Pop() int {
+	l := len(s.items)-1
+	toRemove := s.items[l]
+	s.items = s.items[:l]
+	return toRemove
+}
+
+func main() {
+	myStack := Stack{}
+	fmt.Println(myStack)
+	myStack.Push(100)
+	myStack.Push(200)
+	myStack.Push(300)
+	fmt.Println(myStack)
+	myStack.Pop()
+	fmt.Println(myStack)
+}
